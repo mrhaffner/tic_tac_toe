49757b08804fdcfeb33cba14b7b1ee44eab1b897
Matt Haffner <mrhaffner13@gmail.com>
2020-10-10 18:00:09 -0500

    added in some logic, but it's not working.  does not appear to update anything...not calling the function?

--- script.js
+++ script.js
@@ -20,6 +20,90 @@ const displayController = (() => {
     let turn = 'X';
     //when both players have a name, run the play? function
 
+    //checking for win/lose/draw
+    //should be done after each board update or will it just continuously run????
+        //set win condition logic
+            //??
+            //set each players status to win or lose
+            //make a switch for X and a switch for O
+            //8 ways to win
+            //put the switch in a function to run on whatever event
+                //function will reset the game if gameStatus is not undefined/null
+                    //status, the players, and the turn, and gameBoard.board needs to be = [null, null, null, null, null, null, null, null, null];
+            //gameStatus will refer to player1
+
+    const resetGame = () => {
+        if (!gameStatus === undefined) {
+            //log the results before switching the gamestatus
+            gameBoard.board = [null, null, null, null, null, null, null, null, null];
+            //maybe this should happen from within the gameboard object - addan event listener on on click, check the game status?
+            gameStatus = undefined;
+            turn = 'X';
+            //reset the playerNames
+
+            //reset the square displays
+            emptySquares.forEach((square) => {
+                square.textContent = ''
+            })
+        }
+    };
+
+    const gameState = () => {
+        switch(true) {
+            case gameBoard.board[0] === 'X' && gameBoard.board[1] === 'X' && gameBoard.board[2] === 'X':
+                gameStatus = 'Win';
+                break;
+            case gameBoard.board[3] === 'X' && gameBoard.board[4] === 'X' && gameBoard.board[5] === 'X':
+                gameStatus = 'Win';
+                break;
+            case gameBoard.board[6] === 'X' && gameBoard.board[7] === 'X' && gameBoard.board[8] === 'X':
+                gameStatus = 'Win';
+                break;
+            case gameBoard.board[0] === 'X' && gameBoard.board[3] === 'X' && gameBoard.board[6] === 'X':
+                gameStatus = 'Win';
+                break;
+            case gameBoard.board[1] === 'X' && gameBoard.board[4] === 'X' && gameBoard.board[7] === 'X':
+                gameStatus = 'Win';
+                break;
+            case gameBoard.board[2] === 'X' && gameBoard.board[5] === 'X' && gameBoard.board[8] === 'X':
+                gameStatus = 'Win';
+                break;
+            case gameBoard.board[0] === 'X' && gameBoard.board[4] === 'X' && gameBoard.board[8] === 'X':
+                gameStatus = 'Win';
+                break;
+            case gameBoard.board[2] === 'X' && gameBoard.board[4] === 'X' && gameBoard.board[6] === 'X':
+                gameStatus = 'Win';
+                break;     
+            case gameBoard.board[0] === 'O' && gameBoard.board[1] === 'O' && gameBoard.board[2] === 'O':
+                gameStatus = 'Lose';
+                break;
+            case gameBoard.board[3] === 'O' && gameBoard.board[4] === 'O' && gameBoard.board[5] === 'O':
+                gameStatus = 'Lose';
+                break;
+            case gameBoard.board[6] === 'O' && gameBoard.board[7] === 'O' && gameBoard.board[8] === 'O':
+                gameStatus = 'Lose';
+                break;
+            case gameBoard.board[0] === 'O' && gameBoard.board[3] === 'O' && gameBoard.board[6] === 'O':
+                gameStatus = 'Lose';
+                break;
+            case gameBoard.board[1] === 'O' && gameBoard.board[4] === 'O' && gameBoard.board[7] === 'O':
+                gameStatus = 'Lose';
+                break;
+            case gameBoard.board[2] === 'O' && gameBoard.board[5] === 'O' && gameBoard.board[8] === 'O':
+                gameStatus = 'Lose';
+                break;
+            case gameBoard.board[0] === 'O' && gameBoard.board[4] === 'O' && gameBoard.board[8] === 'O':
+                gameStatus = 'Lose';
+                break;
+            case gameBoard.board[2] === 'O' && gameBoard.board[4] === 'O' && gameBoard.board[6] === 'O':
+                gameStatus = 'Lose';
+                break;
+            case !gameBoard.board.includes(null):
+                gameStatus = 'Draw';
+                break;                                                       
+        };
+    };
+
     const emptySquares = document.querySelectorAll('.square');
 
     emptySquares.forEach((square) => {
@@ -32,77 +116,18 @@ const displayController = (() => {
                 } else {
                     turn = 'O'
                 }
-            }
-        })
-    })
+                //this shit isn't working
+                gameState();
+                resetGame();
+            };
+        });
+    });
     //const play = () => {}
 
-    //checking for win/lose/draw
-        //should be done after each board update or will it just continuously run????
-            //set win condition logic
-                //??
-                //set each players status to win or lose
-                //make a switch for X and a switch for O
-                //8 ways to win
-                //put the switch in a function to run on whatever event
-                    //function will reset the game if gameStatus is not undefined/null
-                        //status, the players, and the turn, and gameBoard.board needs to be = [null, null, null, null, null, null, null, null, null];
-                //gameStatus will refer to player1
-    switch(true) {
-        case gameBoard.board[0] === 'X' && gameBoard.board[1] === 'X' && gameBoard.board[2] === 'X':
-            gameStatus = 'Win';
-            break;
-        case gameBoard.board[3] === 'X' && gameBoard.board[4] === 'X' && gameBoard.board[5] === 'X':
-            gameStatus = 'Win';
-            break;
-        case gameBoard.board[6] === 'X' && gameBoard.board[7] === 'X' && gameBoard.board[8] === 'X':
-            gameStatus = 'Win';
-            break;
-        case gameBoard.board[0] === 'X' && gameBoard.board[3] === 'X' && gameBoard.board[6] === 'X':
-            gameStatus = 'Win';
-            break;
-        case gameBoard.board[1] === 'X' && gameBoard.board[4] === 'X' && gameBoard.board[7] === 'X':
-            gameStatus = 'Win';
-            break;
-        case gameBoard.board[2] === 'X' && gameBoard.board[5] === 'X' && gameBoard.board[8] === 'X':
-            gameStatus = 'Win';
-            break;
-        case gameBoard.board[0] === 'X' && gameBoard.board[4] === 'X' && gameBoard.board[8] === 'X':
-            gameStatus = 'Win';
-            break;
-        case gameBoard.board[2] === 'X' && gameBoard.board[4] === 'X' && gameBoard.board[6] === 'X':
-            gameStatus = 'Win';
-            break;     
-        case gameBoard.board[0] === 'O' && gameBoard.board[1] === 'O' && gameBoard.board[2] === 'O':
-            gameStatus = 'Lose';
-            break;
-        case gameBoard.board[3] === 'O' && gameBoard.board[4] === 'O' && gameBoard.board[5] === 'O':
-            gameStatus = 'Lose';
-            break;
-        case gameBoard.board[6] === 'O' && gameBoard.board[7] === 'O' && gameBoard.board[8] === 'O':
-            gameStatus = 'Lose';
-            break;
-        case gameBoard.board[0] === 'O' && gameBoard.board[3] === 'O' && gameBoard.board[6] === 'O':
-            gameStatus = 'Lose';
-            break;
-        case gameBoard.board[1] === 'O' && gameBoard.board[4] === 'O' && gameBoard.board[7] === 'O':
-            gameStatus = 'Lose';
-            break;
-        case gameBoard.board[2] === 'O' && gameBoard.board[5] === 'O' && gameBoard.board[8] === 'O':
-            gameStatus = 'Lose';
-            break;
-        case gameBoard.board[0] === 'O' && gameBoard.board[4] === 'O' && gameBoard.board[8] === 'O':
-            gameStatus = 'Lose';
-            break;
-        case gameBoard.board[2] === 'O' && gameBoard.board[4] === 'O' && gameBoard.board[6] === 'O':
-            gameStatus = 'Lose';
-            break;
-        case !gameBoard.board.includes(null):
-            gameStatus = 'Draw';
-            break;                                                       
-    }
 
-    return { gameStatus };
+    
+
+    return { gameStatus, turn, resetGame };
 })();
 
 
